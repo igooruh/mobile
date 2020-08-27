76d072f10007b56a29f589dd0d9e4f0a9fdf9116
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-08-27 00:20:48 -0300

    Diferencas do ReactJS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
-    return <View />;
+    return (
+        <>
+            <StatusBar barStyle='light-content' />
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello goStack</Text>
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159C1',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    title: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: 'bold'
+    }
+});
