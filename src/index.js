bea1c9e51992b6d729d0ed8d0dd9958107126b27
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-08-27 16:43:17 -0300

    Listando projetos da API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
     return (
         <>
             <StatusBar barStyle='light-content' />
-            <View style={styles.container}>
-                <Text style={styles.title}>Hello goStack</Text>
-            </View>
+
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
+                        <Text style={styles.project}>{project.title}</Text>
+                    )}
+                />
+            </SafeAreaView>
         </>
     );
 }
@@ -16,13 +33,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#7159C1',
-        justifyContent: 'center',
-        alignItems: 'center'
     },
 
-    title: {
+    project: {
         color: '#FFF',
-        fontSize: 32,
-        fontWeight: 'bold'
+        fontSize: 20,
     }
 });
